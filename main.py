1b1d8336d32d8bb90c728c2efe3ccaa05df8736c
timgaydarov <gaidarov_timur@mail.ru>
2023-06-07 12:03:23 +0300

    add_validation

--- main.py
+++ main.py
@@ -4,24 +4,27 @@ from random import randint
 num = randint(1, 100)
 
 '''Счетчик количества попыток'''
-counter = 1
+counter = 0
 
 while True:
-    user_num = int(input('Я загадал число от 1 до 100, угадай его: '))
-    counter += 1
-    if user_num == num:
-        print(f'Ты угадал! Количество попыток: {counter}')
-        new_game = input('Хочешь сыграть еще? (y/n) ')
-        if new_game == 'y':
-            num = randint(1, 100)
-            counter = 0
-        else:
-            print('Спасибо за игру!')
-            break
-    elif user_num > num:
-        print(f"Загаданное число меньше. Попробуй еще раз!")
+    user_num = input('Я загадал число от 1 до 100, угадай его: ')
+    if not user_num.isdigit():
+        print('Можно ввести только числовое значение')
     else:
-        print(f"Загаданное число больше. Попробуй еще раз!")
-
+        user_num = int(user_num)
+        counter += 1
+        if user_num == num:
+            print(f'Ты угадал! Количество попыток: {counter}')
+            new_game = input('Хочешь сыграть еще? (y/n) ')
+            if new_game == 'y':
+                num = randint(1, 100)
+                counter = 0
+            else:
+                print('Спасибо за игру!')
+                break
+        elif user_num > num:
+            print(f"Загаданное число меньше. Попробуй еще раз!")
+        else:
+            print(f"Загаданное число больше. Попробуй еще раз!")
 
 
